6c3f5c23e5416566bd45d1d9886e20c5f5fa6e4e
Vivek Singh <vividrajput@gmail.com>
2025-10-06 22:48:56 +0530

    adding test.py file

---
+++ test.py
@@ -0,0 +1,11 @@
+def add_numbers(a, b):
+    return a + b
+
+# Example usage
+num1 = 5
+# initialising first number
+num2 = 7
+# initialising second number
+
+result = add_numbers(num1, num2)
+print(f"The sum of {num1} and {num2} is {result}")
