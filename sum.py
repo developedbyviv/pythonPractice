9153fbb6798d4160a1802b9862b58a4425bc6f7a
Aman <aman9760528609@gmail.com>
2025-10-07 23:12:54 +0530

    adding sum file.

---
+++ sum.py
@@ -0,0 +1,10 @@
+# sum.py
+
+def sum_two_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    result = sum_two_numbers(num1, num2)
+    print("The sum is:", result)
